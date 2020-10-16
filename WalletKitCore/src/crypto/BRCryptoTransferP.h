315ceb8be8e8c6bd819212e2016e52c05e6aaad3
Ed Gamble <ed@breadwallet.com>
2020-10-16 09:11:27 -0700

    CORE-1035: In `cryptoListenerGenerate{Transfer,...}Event` check `listener->listener`.

--- WalletKitCore/src/crypto/BRCryptoTransferP.h
+++ WalletKitCore/src/crypto/BRCryptoTransferP.h
@@ -143,7 +143,6 @@ cryptoTransferAttributeArrayRelease (BRArrayOf(BRCryptoTransferAttribute) attrib
 static inline void
 cryptoTransferGenerateEvent (BRCryptoTransfer transfer,
                              BRCryptoTransferEvent event) {
-    if (NULL == transfer->listener.listener) return;
     cryptoListenerGenerateTransferEvent(&transfer->listener, transfer, event);
 }
 
